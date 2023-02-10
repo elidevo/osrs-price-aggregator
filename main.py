a4086251b3fdf5a2d8c88e8fb05d490209810033
Owen <117609185+elidevo@users.noreply.github.com>
2023-02-10 18:11:37 -0500

    Use pandas to create dataframes from data
    This will help make doing aggregations in the future easier.

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
-import json, requests, csv
+import json, requests, csv, pandas
 
 api_url = "https://prices.runescape.wiki/api/v1/osrs"
 headers = {'user-agent': 'elidevo-osrs-price-aggregator-python'}
 
 class price_data:
     def __init__(self):
-        self.data = {}
+        self.__data = {}
 
     def pull_data(self):
         item_mappings = {}
@@ -19,18 +19,14 @@ class price_data:
         res_volumes = requests.get(f"{api_url}/volumes", headers=headers)
         jdata_volumes = json.loads(res_volumes.text)
 
-        for item in jdata_mapping:
-            item_mappings[item["id"]] = item
-            # if not in latest or volumes dont add?? idk
-
-        for id, idict in item_mappings.items():
+        for idict in jdata_mapping:
             try:
-                idict["volume"] = jdata_volumes["data"][str(id)]
+                idict["volume"] = jdata_volumes["data"][str(idict["id"])]
             except:
-                print(f"Problem finding volume data for itemId {str(id)}")
+                print("Problem finding volume data for itemId {}".format(idict["id"]))
 
             try:
-                idata = jdata_latest["data"][str(id)]
+                idata = jdata_latest["data"][str(idict["id"])]
 
                 idict["high"] = idata["high"] # price item is being bought for
                 idict["highTime"] = idata["highTime"] # time in unix millis when high price was gathered
@@ -39,10 +35,15 @@ class price_data:
 
                 idict["margin"] = idict["high"] - idict["low"] # margin between high and low prices (PROFIT MARGIN)
             except:
-                print(f"Problem finding latest data for itemId {str(id)}, popping?...")
+                print("Problem finding latest data for itemId {}, popping?...".format(idict["id"]))
                 #item_mappings.pop(key)
 
-        self.data = item_mappings
+        self.__data = pandas.DataFrame.from_dict(jdata_mapping)
+
+    def get_data(self):
+        return self.__data
 
 pdata = price_data()
 pdata.pull_data()
+
+pdata.get_data().to_excel("data_output.xlsx")
